acf9048290e17645dd2a82e4f58f87f2aba0c352
musiciancoder <andresguitarra1980@gmail.com>
2022-03-04 16:05:21 -0300

    video33 conflicto de rutas

--- WebContent/WEB-INF/vista/paginaEjemplo.jsp
+++ WebContent/WEB-INF/vista/paginaEjemplo.jsp
@@ -8,6 +8,6 @@
 </head>
 <body>
 	<h1>Primer ejemplo de MVC con Spring</h1>
-	<a href="muestraFormulario">Ir al formulario</a>
+	<a href="secundario/muestraFormulario">Ir al formulario</a>
 </body>
 </html>
--- src/es/pildoras/spring/mvc/TirarControlador.java
+++ src/es/pildoras/spring/mvc/TirarControlador.java
@@ -1,32 +1,25 @@
 package es.pildoras.spring.mvc;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("principal")
-public class HolaAlumnosControlador {
+@RequestMapping("secundario")
+public class TirarControlador {
 	
 	@RequestMapping("/muestraFormulario")
 	public String muestraFormulario() { //muestra el formulario
 		   return "HolaAlumnosFormulario";
 	}
 	
-	@RequestMapping("/procesarFormulario")
-	public String procesarFormulario() {//procesa el formulario
-		return "HolaAlumnosSpring";
-	}
-	
 	@RequestMapping("/procesarFormulario2")
 	//public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {//agrega datos al modelo
 	public String otroProcesoFormulario(@RequestParam("nombreAlumno") String nombre, Model modelo) {	
 	//String nombre=request.getParameter("nombreAlumno");
-		nombre+=" es el mejor alumno";
-		String mensajeFinal="¿Quien es el mejor alumno?" + nombre;
+		nombre+=" es el peor alumno";
+		String mensajeFinal="¿Quien es el peor alumno?" + nombre;
 		modelo.addAttribute("mensajeClaro", mensajeFinal);//aca es donde agrega
 		return "HolaAlumnosSpring";
 	}
